77273eec023cf2d19507062679235be01fe635ac
Domingos Savio <domingos.pinheirodonascimentoj@inaf.it>
2025-03-22 00:45:02 +0100

    Removed MPI operations

--- guided_diffusion/logger.py
+++ guided_diffusion/logger.py
@@ -175,9 +175,7 @@ class TensorBoardOutputFormat(KVWriter):
 
         summary = self.tf.Summary(value=[summary_val(k, v) for k, v in kvs.items()])
         event = self.event_pb2.Event(wall_time=time.time(), summary=summary)
-        event.step = (
-            self.step
-        )  # is there any reason why you'd want to specify the step?
+        event.step = self.step
         self.writer.WriteEvent(event)
         self.writer.Flush()
         self.step += 1
@@ -274,14 +272,10 @@ def set_level(level):
     get_current().set_level(level)
 
 
-def set_comm(comm):
-    get_current().set_comm(comm)
-
-
 def get_dir():
     """
     Get directory that log files are being written to.
-    will be None if there is no output directory (i.e., if you didn't call start)
+    Will be None if there is no output directory (i.e., if you didn't call start).
     """
     return get_current().get_dir()
 
@@ -325,22 +319,19 @@ def profile(n):
 def get_current():
     if Logger.CURRENT is None:
         _configure_default_logger()
-
     return Logger.CURRENT
 
 
 class Logger(object):
-    DEFAULT = None  # A logger with no output files. (See right below class definition)
-    # So that you can still log to the terminal without setting up any output files
+    DEFAULT = None  # A logger with no output files.
     CURRENT = None  # Current logger being used by the free functions above
 
-    def __init__(self, dir, output_formats, comm=None):
-        self.name2val = defaultdict(float)  # values this iteration
+    def __init__(self, dir, output_formats):
+        self.name2val = defaultdict(float)  # Values this iteration
         self.name2cnt = defaultdict(int)
         self.level = INFO
         self.dir = dir
         self.output_formats = output_formats
-        self.comm = comm
 
     # Logging API, forwarded
     # ----------------------------------------
@@ -353,22 +344,10 @@ class Logger(object):
         self.name2cnt[key] = cnt + 1
 
     def dumpkvs(self):
-        if self.comm is None:
-            d = self.name2val
-        else:
-            d = mpi_weighted_mean(
-                self.comm,
-                {
-                    name: (val, self.name2cnt.get(name, 1))
-                    for (name, val) in self.name2val.items()
-                },
-            )
-            if self.comm.rank != 0:
-                d["dummy"] = 1  # so we don't get a warning about empty dict
-        out = d.copy()  # Return the dict for unit testing purposes
+        out = self.name2val.copy()
         for fmt in self.output_formats:
             if isinstance(fmt, KVWriter):
-                fmt.writekvs(d)
+                fmt.writekvs(out)
         self.name2val.clear()
         self.name2cnt.clear()
         return out
@@ -381,9 +360,6 @@ class Logger(object):
     # ----------------------------------------
     def set_level(self, level):
         self.level = level
-
-    def set_comm(self, comm):
-        self.comm = comm
 
     def get_dir(self):
         return self.dir
@@ -400,48 +376,9 @@ class Logger(object):
                 fmt.writeseq(map(str, args))
 
 
-def get_rank_without_mpi_import():
-    # check environment variables here instead of importing mpi4py
-    # to avoid calling MPI_Init() when this module is imported
-    for varname in ["PMI_RANK", "OMPI_COMM_WORLD_RANK"]:
-        if varname in os.environ:
-            return int(os.environ[varname])
-    return 0
-
-
-def mpi_weighted_mean(comm, local_name2valcount):
+def configure(dir=None, format_strs=None, log_suffix=""):
     """
-    Copied from: https://github.com/openai/baselines/blob/ea25b9e8b234e6ee1bca43083f8f3cf974143998/baselines/common/mpi_util.py#L110
-    Perform a weighted average over dicts that are each on a different node
-    Input: local_name2valcount: dict mapping key -> (value, count)
-    Returns: key -> mean
-    """
-    all_name2valcount = comm.gather(local_name2valcount)
-    if comm.rank == 0:
-        name2sum = defaultdict(float)
-        name2count = defaultdict(float)
-        for n2vc in all_name2valcount:
-            for (name, (val, count)) in n2vc.items():
-                try:
-                    val = float(val)
-                except ValueError:
-                    if comm.rank == 0:
-                        warnings.warn(
-                            "WARNING: tried to compute mean on non-float {}={}".format(
-                                name, val
-                            )
-                        )
-                else:
-                    name2sum[name] += val * count
-                    name2count[name] += count
-        return {name: name2sum[name] / name2count[name] for name in name2sum}
-    else:
-        return {}
-
-
-def configure(dir=None, format_strs=None, comm=None, log_suffix=""):
-    """
-    If comm is provided, average all numerical stats across that comm
+    Configure logging directory and output format.
     """
     if dir is None:
         dir = os.getenv("OPENAI_LOGDIR")
@@ -454,19 +391,13 @@ def configure(dir=None, format_strs=None, comm=None, log_suffix=""):
     dir = os.path.expanduser(dir)
     os.makedirs(os.path.expanduser(dir), exist_ok=True)
 
-    rank = get_rank_without_mpi_import()
-    if rank > 0:
-        log_suffix = log_suffix + "-rank%03i" % rank
-
     if format_strs is None:
-        if rank == 0:
-            format_strs = os.getenv("OPENAI_LOG_FORMAT", "stdout,log,csv").split(",")
-        else:
-            format_strs = os.getenv("OPENAI_LOG_FORMAT_MPI", "log").split(",")
+        format_strs = os.getenv("OPENAI_LOG_FORMAT", "stdout,log,csv").split(",")
+
     format_strs = filter(None, format_strs)
     output_formats = [make_output_format(f, dir, log_suffix) for f in format_strs]
 
-    Logger.CURRENT = Logger(dir=dir, output_formats=output_formats, comm=comm)
+    Logger.CURRENT = Logger(dir=dir, output_formats=output_formats)
     if output_formats:
         log("Logging to %s" % dir)
 
@@ -484,12 +415,11 @@ def reset():
 
 
 @contextmanager
-def scoped_configure(dir=None, format_strs=None, comm=None):
+def scoped_configure(dir=None, format_strs=None):
     prevlogger = Logger.CURRENT
-    configure(dir=dir, format_strs=format_strs, comm=comm)
+    configure(dir=dir, format_strs=format_strs)
     try:
         yield
     finally:
         Logger.CURRENT.close()
         Logger.CURRENT = prevlogger
-
